511c178b65ef1c83b76abeb8eb52da94356683fb
Matheus <mathewdourado@gmail.com>
2023-06-14 02:05:45 -0300

    resolução do desafio

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -13,45 +13,72 @@ class LoginScreen extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.6,
+            Image.asset(
+              'assets/images/ferrari.jpg',
+              width: MediaQuery.of(context).size.width * 0.5,
               height: double.infinity,
-              child: Image.asset(
-                'assets/images/ferrari.jpg',
-                fit: BoxFit.cover,
-              ),
+              fit: BoxFit.cover,
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/logo.png'),
-                const SizedBox(
-                  width: 250,
-                  child: TextField(
-                    decoration: InputDecoration(hintText: 'Digite seu e-mail'),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/logo.png'),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24.0, bottom: 8.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(4.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(color: Colors.black),
+                      ),
+                      width: 350,
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.person),
+                          hintText: 'Digite seu e-mail',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 250,
-                  child: TextField(
-                      decoration:
-                          InputDecoration(hintText: 'Digite sua senha')),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          fixedSize: const Size(150, 40)),
-                      onPressed: (() {}),
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Icon(Icons.car_crash),
-                          Text('Acessar'),
-                        ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(4.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(color: Colors.black),
+                      ),
+                      width: 350,
+                      child: const TextField(
+                          decoration: InputDecoration(
+                        hintText: 'Digite sua senha',
+                        icon: Icon(Icons.lock),
+                        border: InputBorder.none,
                       )),
-                )
-              ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(150, 40),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        onPressed: (() {}),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(Icons.car_rental),
+                            Text('Acessar'),
+                          ],
+                        )),
+                  )
+                ],
+              ),
             ),
             Container()
           ],
